be83b97ca32246e3605ef73658ba3881397d7a45
Juan-GR123 <juangarros2005@gmail.com>
2025-01-20 14:30:01 +0100

    CrearCategorias_Juan_Inicio

--- COLABBlog-main/blog2/requires/conexion.php
+++ COLABBlog-main/blog2/requires/conexion.php
@@ -3,6 +3,7 @@
     $dsn = "mysql:host=localhost;dbname=blog;charset=utf8mb4";
     $username = "root";
     $password = "";
+    
 try {
     $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
