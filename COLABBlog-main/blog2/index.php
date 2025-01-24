6566b6d5d4937baac773d690b74622f991ce36dd
Juan-GR123 <juangarros2005@gmail.com>
2025-01-24 09:44:25 +0100

    contacto_arreglado

--- COLABBlog-main/blog2/index.php
+++ COLABBlog-main/blog2/index.php
@@ -28,7 +28,7 @@ $_SESSION['loginExito'] = $_SESSION['loginExito'] ?? false;
                 <li><a href="#">Rol</a></li>
                 <li><a href="#">Deportes</a></li>
                 <li><a href="#">Responsabilidad</a></li>
-                <li><a href="#">Contacto</a></li>
+                <li><a href="contacto.php">Contacto</a></li>
             </ul>
         </nav>
     </header>
