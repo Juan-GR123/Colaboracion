696a4e7df65c0919b80405e0dea4ce42e8067065
GRANADRI <adrianmv25@outlook.es>
2025-01-27 09:59:06 +0100

    mejores ids para categorias accion rol deportes y responsabilidad

--- COLABBlog-main/blog2/categoria.php
+++ COLABBlog-main/blog2/categoria.php
@@ -69,10 +69,10 @@ if (isset($_GET['categoria_id'])) {
         <nav id="menu">
             <ul>
                 <li><a href="index.php">Inicio</a></li>
-                <li><a href="categoria.php?categoria_id=1">Acción</a></li>
-                <li><a href="categoria.php?categoria_id=2">Rol</a></li>
-                <li><a href="categoria.php?categoria_id=3">Deportes</a></li>
-                <li><a href="categoria.php?categoria_id=4">Responsabilidad</a></li>
+                <li><a href="categoria.php?categoria_id=2">Acción</a></li>
+                <li><a href="categoria.php?categoria_id=3">Rol</a></li>
+                <li><a href="categoria.php?categoria_id=4">Deportes</a></li>
+                <li><a href="categoria.php?categoria_id=6">Responsabilidad</a></li>
                 <li><a href="contacto.php">Contacto</a></li>
             </ul>
         </nav>
